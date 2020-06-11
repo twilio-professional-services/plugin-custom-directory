f361703db336c7143757b9d0fb640a353f6a1de5
dorkusprime <dorkusprime@users.noreply.github.com>
2020-06-10 19:33:53 -0700

    Adding Serverless Function

---
+++ serverless/functions/getTeamMembers.js
@@ -0,0 +1,71 @@
+exports.handler = async function (context, event, callback) {
+  const client = context.getTwilioClient();
+  const response = new Twilio.Response(); // This is what will be in the eventual HTTP response via the callback method
+  const responseBody = {
+    success: false,
+    payload: {
+      errors: []
+    }
+  } // and this will be the Body of the response
+
+  response.appendHeader('Content-Type', 'application/json');
+  response.appendHeader('Access-Control-Allow-Origin', '*');
+  response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS POST');
+  response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  try {
+    if (Object.keys(event).length === 0) {
+      // This handles the case where NO parameters were sent, allowing for empty Options request (since we don't have access to the Request method/headers)
+      throw {
+        status: 200,
+        code: 60200,
+        message: "No body sent."
+      }
+    }
+
+    if (!event.team_lead_sid) {
+      // This handles the case where a specific parameter was not sent
+      throw {
+        status: 400,
+        code: 60200,
+        message: "Request must include a channelSid"
+      }
+    }
+
+    // First pull our workers with a targetWorkersExpression
+    const workers = await client
+      .taskrouter
+      .workspaces(context.TWILIO_WORKSPACE_SID)
+      .workers
+      .list({
+        targetWorkersExpression: `team_lead_sid = "${event.team_lead_sid}"`
+      });
+
+    // Now let's define our response Worker objects
+    const workerObjects = workers.map((worker) => {
+      return {
+        sid: worker.sid,
+        contact_uri: worker.contact_uri,
+        friendly_name: worker.friendly_name,
+        activity_name: worker.activity_name,
+        attributes: JSON.parse(worker.attributes),
+        available: worker.available
+      }
+    });
+
+    responseBody.success = true;
+    responseBody.payload.workers = workerObjects;
+  } catch (e) {
+    // We've caught an error! Handle the HTTP error response
+    console.error(e.message || e);
+
+    response.setStatusCode(e.status || 500);
+
+    responseBody.success = false;
+    responseBody.payload.errors = responseBody.payload.errors || [];
+    responseBody.payload.errors.push({ code: e.code || 500, message: e.message });
+  }
+
+  response.setBody(responseBody);
+  return callback(null, response);
+}
